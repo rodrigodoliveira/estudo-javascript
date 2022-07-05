b190fa0aa0026f843c6383e5ff95c26fda030ade
Rodrigo Nunes <oliveira.rodrigonunes@gmail.com>
2022-07-05 15:20:22 -0300

    Conversão de tipos

---
+++ ConversoesTipos.js
@@ -0,0 +1,15 @@
+//tipo de dado
+//booleanos
+
+//conversão implicita
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero == numeroString); //conversão implicita
+console.log(numero === numeroString); //conversão explicita
+
+console.log(numero + numeroString);
+console.log(numero + Number(numeroString));
+
+
+//conversão explicita
