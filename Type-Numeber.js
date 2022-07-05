0f5cfd30a25f5fd5fc17cba99074753e9d9c6e81
Rodrigo Nunes <oliveira.rodrigonunes@gmail.com>
2022-07-05 11:40:43 -0300

    exemplos da aula 1

---
+++ Type-Numeber.js
@@ -0,0 +1,6 @@
+const primeiro = 1;
+const segundo = 2;
+
+const operacao = primeiro+segundo;
+
+console.log(operacao);
---
+++ Type-String.js
@@ -0,0 +1,8 @@
+const texto = "Olá mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "SuperSegura123456!";
+
+const cidade = "Belo Horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
