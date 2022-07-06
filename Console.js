bbed105f3e5c3e8ac8023d8bcd1cc9a9ec1c9c13
Rodrigo Nunes <oliveira.rodrigonunes@gmail.com>
2022-07-06 13:00:56 -0300

    Trabalhando com log

---
+++ Console.js
@@ -0,0 +1,11 @@
+//log -> registro no console
+
+const minhaVar = true;
+
+console.log(245);
+console.log("eu sou um texto");
+console.log(minhaVar);
+
+
+//tratamento de erro
+console.error('deu um erro');
