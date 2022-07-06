8d24ce93fefc0aa1ce48b84ec450a3b8d0bf2c24
Rodrigo Nunes <oliveira.rodrigonunes@gmail.com>
2022-07-06 15:11:27 -0300

    Template string

---
+++ TemplateString.js
@@ -0,0 +1,9 @@
+const nome = "Ju";
+const idade = 2022-1982;
+const cidade = "São Paulo"
+const apresentacao = 'Meu nome é ' + nome + ', minha idade é ' + idade + ' e nasci na cidade de ' + cidade; 
+console.log(apresentacao);
+
+
+const apresentacaoTemplate = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidade}`;
+console.log(apresentacaoTemplate)
