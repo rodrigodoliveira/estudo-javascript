4618f39cc56b7533e16eddeaefbd93d6d94b3ad1
Rodrigo Nunes <oliveira.rodrigonunes@gmail.com>
2022-07-06 15:41:31 -0300

    Funções

---
+++ Funcoes.js
@@ -0,0 +1,19 @@
+
+//imprimeTexto('oi');
+
+
+
+
+ //três formas de escrever um função;
+
+
+ //declaração de função (forma mais comum)
+ function imprimeTexto(texto){
+    console.log(texto);
+}
+
+function soma(){
+    return 2+2;
+}
+
+imprimeTexto(soma());
