8616970530c43b359686134d123fa07543b7d160
Rodrigo Nunes <oliveira.rodrigonunes@gmail.com>
2022-07-12 16:35:28 -0300

    Algoritimos

---
+++ Algoritimos/lista-livros.js
@@ -0,0 +1,11 @@
+
+const precosLivros = [
+    { titulo: "Javascript", preco: 25 },
+    { titulo: "PHP", preco: 15 },
+    { titulo: "Java", preco: 30 },
+    { titulo: "Elixir", preco: 50 },
+    { titulo: "Go", preco: 45 },
+    { titulo: "Phyton", preco: 20 }
+];
+
+module.exports = precosLivros
---
+++ Algoritimos/selection-sort.js
@@ -0,0 +1,16 @@
+const livros = require('./lista-livros');
+const menorValor = require('./menor-valor');
+
+
+for (let atual = 0; atual < livros.length - 1; atual++) {
+
+    let menor = menorValor(livros, atual);
+    let livroAtual = livros[atual];
+    let livroMenorPreco = livros[menor];
+
+    livros[atual] = livroMenorPreco;
+    livros[menor] = livroAtual;
+
+}
+
+console.log(livros);
